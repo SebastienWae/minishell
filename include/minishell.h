91bc8c552f8a8a2c74978d19cf341be6a2bccbae
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-04-29 12:18:17 +0200

    Add tokenizer

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: seb <seb@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 14:48:00 by swaegene          #+#    #+#             */
-/*   Updated: 2022/04/29 14:32:30 by swaegene         ###   ########.fr       */
+/*   Updated: 2022/04/29 14:33:20 by swaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,12 @@
 typedef struct s_cmd		t_cmd;
 typedef struct s_in			t_in;
 typedef struct s_out		t_out;
+typedef struct s_token		t_token;
 typedef enum e_in_type		t_in_type;
 typedef enum e_out_type		t_out_type;
+typedef enum e_token_type	t_token_type;
 typedef union u_data		t_data;
+typedef union u_token_data	t_token_data;
 
 enum	e_in_type {
 	IN_PIPE,
@@ -33,10 +36,18 @@ enum	e_out_type {
 	OUT_FILE_APPEND,
 	STD
 };
+enum e_token_type {
+	METACHARACTER,
+	WORD
+};
 
 union u_data {
 	t_cmd	*pipe_src;
 	char	*path;
+};
+union u_token_data {
+	char	*word;
+	char	c;
 };
 
 struct s_cmd {
@@ -52,9 +63,16 @@ struct s_out {
 	enum e_out_type	type;
 	union u_data	*data;
 };
+struct s_token {
+	enum e_token_type	type;
+	union u_token_data	data;
+};
 
-int	is_metacharacter(char c);
-int	is_whitespace(char c);
-int	is_quote(char c);
+t_list	*tokenizer(char *line);
+void	free_tokens(t_list *tokens);
+
+int		is_quote(char c);
+int		is_metacharacter(char c);
+int		is_whitespace(char c);
 
 #endif
