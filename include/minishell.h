dd9dfeaa947ee24654f6affc25122d8c9d3056bd
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-05-03 13:54:17 +0200

    Reorder tokenizer and headers

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: swaegene <swaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 14:48:00 by swaegene          #+#    #+#             */
-/*   Updated: 2022/05/02 14:22:35 by swaegene         ###   ########.fr       */
+/*   Updated: 2022/05/03 13:46:33 by swaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,65 +14,10 @@
 # define MINISHELL_H
 
 # include <libft.h>
-# include "pipex.h"
 
 # define SHELL_NAME "minishell"
 
-typedef struct s_cmd		t_cmd;
-typedef struct s_in			t_in;
-typedef struct s_out		t_out;
-typedef struct s_token		t_token;
 typedef struct s_elem_env	t_elem_env;
-typedef enum e_in_type		t_in_type;
-typedef enum e_out_type		t_out_type;
-typedef enum e_token_type	t_token_type;
-typedef union u_data		t_data;
-typedef union u_token_data	t_token_data;
-
-enum	e_in_type {
-	IN_PIPE,
-	IN_FILE,
-	HEREDOC
-};
-enum	e_out_type {
-	OUT_PIPE,
-	OUT_FILE,
-	OUT_FILE_APPEND,
-	STD
-};
-enum e_token_type {
-	METACHARACTER,
-	SINGLE_QUOTE,
-	DOUBLE_QUOTE,
-	WORD
-};
-
-union u_data {
-	t_cmd	*pipe_src;
-	char	*path;
-};
-union u_token_data {
-	char	*word;
-	char	c;
-};
-
-struct s_cmd {
-	char	**cmd;
-	t_list	*in;
-	t_out	*out;
-};
-struct s_in {
-	enum e_in_type	type;
-	union u_data	*data;
-};
-struct s_out {
-	enum e_out_type	type;
-	union u_data	*data;
-};
-struct s_token {
-	enum e_token_type	type;
-	union u_token_data	data;
-};
 
 struct	s_elem_env
 {
@@ -83,6 +28,7 @@ struct	s_elem_env
 int		is_quote(char c);
 int		is_metacharacter(char c);
 int		is_whitespace(char c);
+
 int		ft_strcmp(char *str1, char *str2);
 int		ft_execute_sys_cmd(char **cmd, char **env);
 int		ft_is_builtin_cmd(char *str);
@@ -94,8 +40,10 @@ int		ft_cd(char **cmd);
 int		ft_env(char **cmd, t_list *g_local_env);
 void	ft_print_env(t_list *g_local_env, int choice);
 void	ft_check_arg_error(int argc);
-void	free_tokens(t_list *tokens);
+
 t_list	*tokenizer(char *line);
+void	free_tokens(t_list *tokens);
+
 t_list	*ft_unset(char **cmd, t_list *g_local_env);
 t_list	*ft_export(char **cmd, t_list *g_local_env);
 t_list	*ft_execute_builtin_cmd(char **parsed_str, t_list *g_local_env);
--- include/tokenizer.h
+++ include/tokenizer.h
@@ -6,7 +6,7 @@
 /*   By: swaegene <swaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 15:43:39 by swaegene          #+#    #+#             */
-/*   Updated: 2022/04/29 17:48:57 by swaegene         ###   ########.fr       */
+/*   Updated: 2022/05/03 13:51:44 by swaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,5 @@
 
 # include <minishell.h>
 # include <libft.h>
-
-typedef struct s_quote	t_quote;
-
-struct s_quote {
-	t_token_type	type;
-	int				n;
-};
-
-t_list	*add_token(t_token_type type, t_token_data data, t_list *tokens);
-void	add_metacharacter(char *c, t_list **tokens);
-void	add_word(char *word, t_list **tokens);
-void	end_quote(char *quote, t_quote *quotes);
-void	start_quote(char *quote, t_quote *quotes, t_list **tokens);
 
 #endif
