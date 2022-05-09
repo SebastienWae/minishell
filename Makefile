101a5a7f016eb0759375a2f6a0fb7248658da17c
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-05-09 13:42:55 +0200

    Fixes makefiles, headers and remove unused functions

--- Makefile
+++ Makefile
@@ -3,46 +3,54 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: swaegene <swaegene@student.42.fr>          +#+  +:+       +#+         #
+#    By: seb <seb@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/28 13:49:18 by swaegene          #+#    #+#              #
-#    Updated: 2022/05/03 13:53:08 by swaegene         ###   ########.fr        #
+#    Updated: 2022/05/09 13:15:25 by swaegene         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = minishell
 
 RM = rm -f
-MKDIR = mkdir
+MKDIR = mkdir -p
 
 LIBFT = libft
 
-SRC_DIR = ./src/
-
 ifdef MAKE_DEBUG
-OUT_DIR = ./debug/
-NAME := $(OUT_DIR)$(NAME)
-DIRS = $(OUT_DIR)
+OUT_DIR = debug
 CFLAGS = -g3 -fsanitize=address
+NAME = minishell_debug
 else
-OUT_DIR = ./
+OUT_DIR = out
 endif
+DIRS = $(OUT_DIR)/src $(OUT_DIR)/src/tokenizer $(OUT_DIR)/src/tokenizer/handlers
 
 CC = gcc
 CFLAGS += -Wall -Wextra -Werror
-CPPFLAGS = -I$(LIBFT) -Iinclude
-LDFLAGS = -L$(LIBFT) -lft
+CPPFLAGS = -I$(LIBFT) -Iinclude -I/goinfre/$(USER)/.brew/opt/readline/include
+LDFLAGS = -L$(LIBFT) -L/goinfre/$(USER)/.brew/opt/readline/lib -lreadline -lft
 
-SRCS = main.c strings.c
-OBJS = $(addprefix $(OUT_DIR),$(SRCS:%.c=%.o))
+SRCS = src/main.c src/strings.c src/debug.c \
+	src/tokenizer/tokenizer.c \
+	src/tokenizer/tokenizer_actions.c \
+	src/tokenizer/handlers/char_handler.c \
+	src/tokenizer/handlers/double_quote_handler.c \
+	src/tokenizer/handlers/end_handler.c \
+	src/tokenizer/handlers/great_handler.c \
+	src/tokenizer/handlers/less_handler.c \
+	src/tokenizer/handlers/pipe_handler.c \
+	src/tokenizer/handlers/single_quote_handler.c \
+	src/tokenizer/handlers/whitespace_handler.c
+OBJS = $(addprefix $(OUT_DIR)/,$(SRCS:%.c=%.o))
 
 all: $(NAME)
 
 $(NAME): $(DIRS) $(OBJS) $(LIBFT)/libft.a
 	$(CC) $(CFLAGS) $(OBJS) $(LDFLAGS) -o $@
 
-$(OUT_DIR)%.o: $(SRC_DIR)%.c
-	$(CC) $(CFLAGS) $(CPPFLAGS) $< -c -o $@
+$(OUT_DIR)/%.o: %.c
+	$(CC) $(CFLAGS) $(CPPFLAGS) -c $< -o $@
 
 $(LIBFT)/libft.a:
 	$(MAKE) -C $(LIBFT) bonus
@@ -62,6 +70,6 @@ fclean: clean
 re: fclean all
 
 compile_commands: $(LIBFT)/libft.a
-	bear -- make re	
+	bear -- make re
 
 .PHONY: all clean fclean re $(LIBFT) compile_commands
--- include/minishell.h
+++ include/minishell.h
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: swaegene <swaegene@student.42.fr>          +#+  +:+       +#+        */
+/*   By: seb <seb@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 14:48:00 by swaegene          #+#    #+#             */
-/*   Updated: 2022/05/03 13:46:33 by swaegene         ###   ########.fr       */
+/*   Updated: 2022/05/07 22:02:15 by seb              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <tokenizer.h>
 # include <libft.h>
 
 # define SHELL_NAME "minishell"
@@ -25,27 +26,27 @@ struct	s_elem_env
 	char	*content;
 };
 
-int		is_quote(char c);
-int		is_metacharacter(char c);
-int		is_whitespace(char c);
+/* Checks if the character is considered to be a whitespace.
+Returns 1 if true, 0 if false. */
+int			is_whitespace(char c);
 
-int		ft_strcmp(char *str1, char *str2);
-int		ft_execute_sys_cmd(char **cmd, char **env);
-int		ft_is_builtin_cmd(char *str);
-int		ft_check_char_export(char *str);
-int		ft_equal_is_in(char *str);
-int		ft_pwd(char **cmd);
-int		ft_echo(char **cmd);
-int		ft_cd(char **cmd);
-int		ft_env(char **cmd, t_list *g_local_env);
-void	ft_print_env(t_list *g_local_env, int choice);
-void	ft_check_arg_error(int argc);
+/* Split and classify the given string.*/
+t_tokenizer	*tokenize(char *line);
 
-t_list	*tokenizer(char *line);
-void	free_tokens(t_list *tokens);
+int			ft_strcmp(char *str1, char *str2);
+int			ft_execute_sys_cmd(char **cmd, char **env);
+int			ft_is_builtin_cmd(char *str);
+int			ft_check_char_export(char *str);
+int			ft_equal_is_in(char *str);
+int			ft_pwd(char **cmd);
+int			ft_echo(char **cmd);
+int			ft_cd(char **cmd);
+int			ft_env(char **cmd, t_list *g_local_env);
+void		ft_print_env(t_list *g_local_env, int choice);
+void		ft_check_arg_error(int argc);
 
-t_list	*ft_unset(char **cmd, t_list *g_local_env);
-t_list	*ft_export(char **cmd, t_list *g_local_env);
-t_list	*ft_execute_builtin_cmd(char **parsed_str, t_list *g_local_env);
+t_list		*ft_unset(char **cmd, t_list *g_local_env);
+t_list		*ft_export(char **cmd, t_list *g_local_env);
+t_list		*ft_execute_builtin_cmd(char **parsed_str, t_list *g_local_env);
 
 #endif
