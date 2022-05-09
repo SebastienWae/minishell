456d4609c0386c0fffaf69192715ddc4dd97b37e
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-05-09 13:42:55 +0200

    Fixes makefiles, headers and remove unused functions

--- src/strings.c
+++ src/strings.c
@@ -6,33 +6,13 @@
 /*   By: seb <seb@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 10:06:36 by seb               #+#    #+#             */
-/*   Updated: 2022/04/29 14:29:25 by swaegene         ###   ########.fr       */
+/*   Updated: 2022/05/07 22:00:11 by seb              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* Checks if the character is considered to be a metacharacter.
-Returns 1 if true, 0 if false */
-int	is_metacharacter(char c)
-{
-	if (c == '|' || c == '<' || c == '>')
-		return (1);
-	return (0);
-}
-
-/* Checks if the character is considered to be a whitespace.
-Returns 1 if true, 0 if false */
 int	is_whitespace(char c)
 {
-	if (c == ' ' || c == '\t' || c == '\n')
-		return (1);
-	return (0);
-}
-
-/* Checks if the character is considered to be a quote.
-Returns 1 if true, 0 if false */
-int	is_quote(char c)
-{
-	if (c == '"' || c == '\'')
+	if (c == ' ' || c == '\t')
 		return (1);
 	return (0);
 }
