e0aaf71f8bf0438675b148bd14c4c610d69c0b34
Jenny Eulliot <jeulliot@k0r1p7.42mulhouse.fr>
2022-05-11 12:07:38 +0200

    removed useless headers, added sys_call.h and updated minishell.h

---
+++ include/built_in_functions.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   built_in_functions.h                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeulliot <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/03 16:20:47 by jeulliot          #+#    #+#             */
+/*   Updated: 2022/05/03 16:20:49 by jeulliot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUILT_IN_FUNCTIONS_H
+# define BUILT_IN_FUNCTIONS_H
+
+# include <minishell.h>
+# include <libft.h>
+
+typedef struct s_unset {
+	t_list	*tmp;
+	t_list	*local_env;
+}	t_unset;
+
+int		ft_is_builtin_cmd(char *str);
+t_list	*ft_execute_builtin_cmd(char **parsed_str, t_list *local_env);
+
+/* Environment handling functions */
+
+int		ft_equal_is_in(char *str);
+int		ft_len_name_elem(char *elem);
+int		ft_is_already_in_env(t_list *local_env, char *elem);
+int		ft_print_env(t_list *local_env, int choice);
+int		ft_check_char_export(char *str);
+void	ft_env(char **cmd, t_list *local_env);
+char	*ft_get_env_var_value(char *elem);
+t_list	*ft_unset(char **cmd, t_list *local_env);
+t_list	*ft_check_next(char **cmd, t_list *local_env, int nb_cmd);
+t_list	*ft_export(char **cmd, t_list *local_env);
+
+/* Others built-in functions */
+
+int		ft_exit(char **str, t_minishell shell);
+void	ft_pwd(char **cmd);
+void	ft_echo(char **cmd);
+void	ft_cd(char **cmd, t_list *local_env);
+
+#endif
--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seb <seb@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: swaegene <swaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 14:48:00 by swaegene          #+#    #+#             */
-/*   Updated: 2022/05/07 22:02:15 by seb              ###   ########.fr       */
+/*   Updated: 2022/04/29 14:49:53 by swaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,30 @@
 
 # include <tokenizer.h>
 # include <libft.h>
+# include <termios.h>
 
 # define SHELL_NAME "minishell"
 
-typedef struct s_elem_env	t_elem_env;
+/* Global variable to save commands exit code */
+int	g_out;
 
-struct	s_elem_env
-{
+typedef struct s_elem_env {
 	char	*name;
 	char	*content;
-};
+}	t_elem_env;
+
+/* sets input and output for the current command */
+typedef struct s_fd_in_out {
+	int	in;
+	int	out;
+}	t_fd_in_out;
+
+typedef struct s_minishell {
+	t_list			*local_env;
+	struct termios	config;
+	int				saved_stdin;
+	int				saved_stdout;
+}	t_minishell;
 
 /* Checks if the character is considered to be a whitespace.
 Returns 1 if true, 0 if false. */
@@ -33,20 +47,15 @@ int			is_whitespace(char c);
 /* Split and classify the given string.*/
 t_tokenizer	*tokenize(char *line);
 
+/* Some useful string functions not available in libft */
 int			ft_strcmp(char *str1, char *str2);
-int			ft_execute_sys_cmd(char **cmd, char **env);
-int			ft_is_builtin_cmd(char *str);
-int			ft_check_char_export(char *str);
-int			ft_equal_is_in(char *str);
-int			ft_pwd(char **cmd);
-int			ft_echo(char **cmd);
-int			ft_cd(char **cmd);
-int			ft_env(char **cmd, t_list *g_local_env);
-void		ft_print_env(t_list *g_local_env, int choice);
-void		ft_check_arg_error(int argc);
+char		*ft_strncpy(char *src, size_t index, size_t end);
 
-t_list		*ft_unset(char **cmd, t_list *g_local_env);
-t_list		*ft_export(char **cmd, t_list *g_local_env);
-t_list		*ft_execute_builtin_cmd(char **parsed_str, t_list *g_local_env);
+/* Need this one for now but will be replaced by libft split later */
+char		**ft_better_split(char *s, char c);
+
+/* Command launching functions */
+t_minishell	ft_pipe(t_minishell shell, char **env);
+t_minishell	ft_launch_cmd(char *str, t_minishell shell, char **env);
 
 #endif
