228fb767b02ad355906b4ddadb29b6e134e8807b
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-05-11 09:14:47 +0200

    save

--- test/parser.test.c
+++ test/parser.test.c
@@ -6,7 +6,7 @@
 /*   By: seb <seb@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 08:08:38 by seb               #+#    #+#             */
-/*   Updated: 2022/05/11 08:38:10 by seb              ###   ########.fr       */
+/*   Updated: 2022/05/11 09:14:30 by seb              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,6 +81,8 @@ static void	parser_redir_in(void **state)
 	tokens = tokenize(line);
 	parser = parse(tokens);
 	cmd = parser->cmds;
+	assert_int_equal(((t_cmd *)(cmd->content))->piped, 0);
+	assert_null(((t_cmd *)(cmd->content))->out);
 	in = ((t_cmd *)(cmd->content))->in;
 	assert_string_equal(((t_cmd *)(cmd->content))->cmd, "echo 123");
 	assert_int_equal(((t_redir *)(in->content))->type, P_RT_IN);
@@ -94,8 +96,74 @@ static void	parser_redir_in(void **state)
 	assert_int_equal(((t_redir *)(in->content))->type, P_RT_IN);
 	assert_string_equal(((t_redir *)(in->content))->target, "in");
 	assert_null(in->next);
-	assert_null(((t_cmd *)(cmd->content))->out);
+	cmd = cmd->next;
+	assert_null(cmd);
+	tokens->destructor(tokens);
+	parser->destructor(parser);
+}
+
+static void	parser_redir_out(void **state)
+{
+	t_list		*cmd;
+	t_list		*out;
+	t_tokenizer	*tokens;
+	t_parser	*parser;
+	char		line[] = "> out echo >>append 123 >out";
+
+	(void)state;
+	tokens = tokenize(line);
+	parser = parse(tokens);
+	cmd = parser->cmds;
 	assert_int_equal(((t_cmd *)(cmd->content))->piped, 0);
+	assert_null(((t_cmd *)(cmd->content))->in);
+	out = ((t_cmd *)(cmd->content))->out;
+	assert_string_equal(((t_cmd *)(cmd->content))->cmd, "echo 123");
+	assert_int_equal(((t_redir *)(out->content))->type, P_RT_OUT);
+	assert_string_equal(((t_redir *)(out->content))->target, "out");
+	assert_non_null(out->next);
+	out = out->next;
+	assert_int_equal(((t_redir *)(out->content))->type, P_RT_APPEND);
+	assert_string_equal(((t_redir *)(out->content))->target, "append");
+	assert_non_null(out->next);
+	out = out->next;
+	assert_int_equal(((t_redir *)(out->content))->type, P_RT_OUT);
+	assert_string_equal(((t_redir *)(out->content))->target, "out");
+	assert_null(out->next);
+	cmd = cmd->next;
+	assert_null(cmd);
+	tokens->destructor(tokens);
+	parser->destructor(parser);
+}
+
+static void	parser_redir_pipe(void **state)
+{
+	t_list		*cmd;
+	t_list		*in;
+	t_list		*out;
+	t_tokenizer	*tokens;
+	t_parser	*parser;
+	char		line[] = "echo 123 | cat < in > out";
+
+	(void)state;
+	tokens = tokenize(line);
+	parser = parse(tokens);
+	cmd = parser->cmds;
+	assert_int_equal(((t_cmd *)(cmd->content))->piped, 1);
+	assert_null(((t_cmd *)(cmd->content))->in);
+	assert_null(((t_cmd *)(cmd->content))->out);
+	assert_string_equal(((t_cmd *)(cmd->content))->cmd, "echo 123");
+	cmd = cmd->next;
+	assert_non_null(cmd);
+	assert_int_equal(((t_cmd *)(cmd->content))->piped, 0);
+	in = ((t_cmd *)(cmd->content))->in;
+	out = ((t_cmd *)(cmd->content))->out;
+	assert_string_equal(((t_cmd *)(cmd->content))->cmd, "cat");
+	assert_int_equal(((t_redir *)(in->content))->type, P_RT_IN);
+	assert_string_equal(((t_redir *)(in->content))->target, "in");
+	assert_null(in->next);
+	assert_int_equal(((t_redir *)(out->content))->type, P_RT_OUT);
+	assert_string_equal(((t_redir *)(out->content))->target, "out");
+	assert_null(out->next);
 	cmd = cmd->next;
 	assert_null(cmd);
 	tokens->destructor(tokens);
@@ -109,6 +177,8 @@ int	main(void)
 		cmocka_unit_test(parser_whitespaces),
 		cmocka_unit_test(parser_simple),
 		cmocka_unit_test(parser_redir_in),
+		cmocka_unit_test(parser_redir_out),
+		cmocka_unit_test(parser_redir_pipe),
 	};
 
 	return (cmocka_run_group_tests(parser_tests, NULL, NULL));
