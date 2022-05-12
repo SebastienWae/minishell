3e9923c67eed94cc6d09fa9ee0be33296a59c402
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-05-12 17:38:23 +0200

    save

--- src/expand/expand_handlers.c
+++ src/expand/expand_handlers.c
@@ -6,28 +6,72 @@
 /*   By: swaegene <swaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 15:10:49 by swaegene          #+#    #+#             */
-/*   Updated: 2022/05/12 17:30:45 by swaegene         ###   ########.fr       */
+/*   Updated: 2022/05/12 17:38:06 by swaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
-#include <utils.h>
 #include <expand.h>
+#include <libft.h>
 #include <stdlib.h>
+#include <utils.h>
 
 void	expand_var_handler(t_expand *e)
 {
+	char	*tmp;
+	char	*var;
+
 	if (e->state == E_S_EXPANDING)
 	{
-
+		if ((e->flags & E_VARIABLE) > 0)
+		{
+			if (e->variable)
+			{
+				var = ft_substr(e->str, e->variable->start, e->variable->end);
+				tmp = e->result;
+				e->result = ft_strjoin(e->result, var);
+				free(tmp);
+				free(e->variable);
+				e->variable = NULL;
+			}
+			e->variable = variable_constructor(e->cursor + 1, e->cursor + 1);
+		}
+		else
+		{
+			tmp = e->result;
+			e->result = ft_strappend(e->result, e->str[e->cursor]);
+			free(tmp);
+		}
+		e->cursor++;
 	}
 	else if (e->state == E_S_IN_SINGLE_QUOTE)
 	{
-
+		tmp = e->result;
+		e->result = ft_strappend(e->result, e->str[e->cursor]);
+		free(tmp);
+		e->cursor++;
 	}
 	else if (e->state == E_S_IN_DOUBLE_QUOTE)
 	{
-		
+		if ((e->flags & E_VARIABLE) > 0)
+		{
+			if (e->variable)
+			{
+				var = ft_substr(e->str, e->variable->start, e->variable->end);
+				tmp = e->result;
+				e->result = ft_strjoin(e->result, var);
+				free(tmp);
+				free(e->variable);
+				e->variable = NULL;
+			}
+			e->variable = variable_constructor(e->cursor + 1, e->cursor + 1);
+		}
+		else
+		{
+			tmp = e->result;
+			e->result = ft_strappend(e->result, e->str[e->cursor]);
+			free(tmp);
+		}
+		e->cursor++;
 	}
 }
 
