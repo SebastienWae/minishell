d4d470a078c3e56394deac9b41061857b753eef6
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-05-18 13:18:56 +0200

    replace get next line

--- include/utils.h
+++ include/utils.h
@@ -6,7 +6,7 @@
 /*   By: swaegene <swaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 14:57:31 by swaegene          #+#    #+#             */
-/*   Updated: 2022/05/17 15:05:09 by swaegene         ###   ########.fr       */
+/*   Updated: 2022/05/18 13:13:14 by swaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,8 @@ char	*ft_strncpy(char *src, size_t index, size_t end);
 char	*ft_strappend(const char *s1, const char c);
 char	*ft_strjoin_sep(const char *s1, const char *s2, const char *sep);
 
-char	*ft_copy_temp(char *str);
-char	*ft_fill_line(char *tmp);
 char	*get_next_line(int fd);
-char	*ft_strcat(char *dest, char *s1, char *s2);
-int		ft_is_a_line(char *s);
-char	*ft_charge_buffer(int fd, char *buf, char *tmp, char *next);
+char	*get_first_line(char **s);
 
 t_array	*array_constructor(void);
 void	array_push(t_array *arr, char *str);
