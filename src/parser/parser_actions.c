9cedc175db75a4869270f1b2c4a6b83a5f93d1c0
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-05-22 19:36:53 +0200

    Send syntax error message to stderr

--- src/parser/parser_actions.c
+++ src/parser/parser_actions.c
@@ -6,10 +6,11 @@
 /*   By: seb <seb@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 09:51:15 by seb               #+#    #+#             */
-/*   Updated: 2022/05/21 07:53:32 by seb              ###   ########.fr       */
+/*   Updated: 2022/05/22 19:36:40 by seb              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <libft.h>
 #include <parser.h>
 #include <stdio.h>
 
@@ -22,8 +23,9 @@ void	parser_pipe_cmd(t_parser *p)
 
 void	parser_syntax_error(t_parser *p)
 {
-	printf(SHELL_NAME ": syntax error near unexpected token `%s'\n",
-		((t_token *)p->tokens->content)->str);
+	ft_putstr_fd(SHELL_NAME ": syntax error near unexpected token ", 2);
+	ft_putstr_fd(((t_token *)p->tokens->content)->str, 2);
+	ft_putstr_fd("\n", 2);
 	parser_end_cmd(p);
 	p->state = P_S_ERROR;
 }
