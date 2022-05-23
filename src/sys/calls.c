d40fb87197c4e906e5546068625bf699f5b0487b
Ptitjen <97309340+Ptitjen@users.noreply.github.com>
2022-05-23 14:21:40 +0200

    fixed exit (#50)
    Co-authored-by: Jenny Eulliot <jeulliot@k0r1p7.42mulhouse.fr>
    Co-authored-by: Sebastien <55792872+SebastienWae@users.noreply.github.com>

--- src/sys/calls.c
+++ src/sys/calls.c
@@ -3,13 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   calls.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seb <seb@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: jeulliot <jeulliot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/28 14:44:44 by jeulliot          #+#    #+#             */
-/*   Updated: 2022/05/23 13:14:21 by swaegene         ###   ########.fr       */
+/*   Updated: 2022/05/23 11:35:11 by jeulliot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
 #include <string.h>
 #include <signal.h>
 #include <sys.h>
